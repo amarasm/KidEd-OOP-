ea0f9fe47a4b91c56bde912a4c21675322c57ab0
amarasm <48725326+amarasm@users.noreply.github.com>
2019-12-03 20:43:49 +0400

    Update KidEdColorsClass.py

--- KidEdColorsClass.py
+++ KidEdColorsClass.py
@@ -1,30 +1,78 @@
 import json
 import random
 
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def setHead(self, name, isprimarycolor):
+        self.head = Color(name, isprimarycolor)
+
+    def appending(self, name, isprimarycolor):
+        temp = self.head
+        if temp is None:
+            self.setHead(name, isprimarycolor)
+        else:
+            while temp.next is not None:
+                temp = temp.next
+            newColor = Color(name, isprimarycolor)
+            temp.next = newColor
+
+    def displayData(self, key):
+        printColor = self.head
+        while printColor is not None:
+            if printColor.name == key:
+                print("\nThe color is", printColor.name)
+                if printColor.isprimarycolor is True:
+                    print("And it's a primary color")
+                    break
+                else:
+                    print("And it's not a primary color")
+                    break
+            else:
+                printColor = printColor.next
+
+    def createCategory(self):
+        names = []
+        keys = self.head
+        while keys is not None:
+            names.append(keys.name)
+            keys = keys.next
+        return names
 
 class Colors:
 
     def __init__(self):
-        self.category = {}
+        self.category = LinkedList()
         self.loadFromJson()
 
     def loadFromJson(self):
         with open('Colors.json') as data_file:
             file = json.load(data_file)
         colorsCateg = file["colors"]
-        self.category = colorsCateg
+        return colorsCateg
+
+    def appendingNewColor(self):
+        colorsCateg = self.loadFromJson()
+        for key in colorsCateg:
+            self.category.appending(colorsCateg[key]["name"], colorsCateg[key]["isPrimaryColor"])
+
+    def getColorNames(self):
+        temp = self.category
+        print((", ").join(temp.createCategory()))
 
     def getColors(self, count):
-        colorList = []
-        for keys in self.category.keys():
-            colorList.append(keys)
-        yourchoices = random.choices(colorList, k=count)
+        categ = self.category.createCategory()
+        yourchoices = random.choices(categ, k=count)
         for key in yourchoices:
-            print(key)
+            self.category.displayData(key)
 
-    def getColorsCateg(self):
-        print((", ").join(self.category.keys()))
+class Color:
 
+    def __init__(self, name, isprimarycolor):
+        self.name = name
+        self.isprimarycolor = isprimarycolor
+        self.next = None
 
 def main():
     print("\nDear user welcome to this application. Here you can learn more about colors.")
@@ -35,10 +83,12 @@ def main():
         if choice == "yes":
             colors = Colors()
 
-            print("\nHere are the keys:")
-            colors.getColorsCateg()
+            colors.appendingNewColor()
 
-            number = 0
+            print("\nHere are the keys:")
+            colors.getColorNames()
+
+
             while True:
                 number = input("\nNumber of colors you want to learn today:")
                 if number.isnumeric():
